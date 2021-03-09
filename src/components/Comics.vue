9ecf45c1cce1065628850ef0e14cd73a887cb396
jmBustaf <josemiguelbf8@gmail.com>
2021-03-08 19:13:51 -0500

    Mejora la logica del componente Comics

--- src/components/Comics.vue
+++ src/components/Comics.vue
@@ -2,10 +2,13 @@
 
     <div class="container p-0 w-100">
         <div class="row py-4 m-0 w-100 d-flex justify-content-around">
-            <div v-for="(item, index) in this.comics.nombre" v-bind:key="index" class="col-3 comic p-0 my-3 m-0"> 
-                <router-link @click="getLink" v-bind:comics="this.comics.detalle[index]" v-bind:to="`/${this.comics.nombre[index]}`"><img class="mx-4 mb-4 w-75 h-75 rounded" v-bind:src="this.comics.image[index]" alt="img-comic"></router-link>
-                <p class="mx-4 m-0">nombre: {{this.comics.nombre[index]}}</p>
-                <p class="mx-4 m-0">fecha: {{this.comics.fecha[index]}}</p>
+            <div v-for="(item, index) in this.comics" v-bind:key="index" class="col-3 comic p-0 my-3 m-0"> 
+                <img class="mx-4 mb-4 w -75 h-75 rounded" v-bind:src="item.image.original_url" alt="img-comic">
+                <!-- <router-link @click="getLink" v-bind:comics="this.comics.detalle[index]" v-bind:to="`/${this.comics.nombre[index]}`">
+                    
+                </router-link> -->
+                <p class="mx-4 m-0">nombre: {{item.name}}</p>
+                <p class="mx-4 m-0">fecha: {{item.date}}</p>
             </div>
         </div>
     </div>
@@ -36,7 +39,7 @@ export default {
             })
             .then(res => res.json())
             .then(data => {
-                let arrayImage = []
+                /* let arrayImage = []
                 let arrayFechas = []
                 let arrayNombres = []
                 let arrayDetalle = []
@@ -53,6 +56,9 @@ export default {
                     this.comics.detalle.push(element.api_detail_url + '?api_key=6f7e42c1a04a1903ca5c2a635e441781e12a537b&format=json')
                 }); 
                 this.comics.push(data.results)
+                 */
+
+                this.comics = data.results;
                 console.log(this.comics)
             })
             .catch(error => console.log(error))
